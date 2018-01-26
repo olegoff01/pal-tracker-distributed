28718f5b531d6b927278bc5716deca68b4be989c
Eric Reece <reeceer@gmail.com>
2018-01-26 09:22:30 -0600

    add component scan to euraka server app

--- platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
+++ platform-services/eureka-server/src/main/java/io/pivotal/pal/tracker/eurekaserver/App.java
@@ -3,9 +3,11 @@ package io.pivotal.pal.tracker.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaServer
 @SpringBootApplication
+@ComponentScan("io.pivotal.pal.tracker.eurekaserver")
 public class App {
 
     public static void main(String[] args) {
